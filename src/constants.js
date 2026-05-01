145fe16572523069a0b77cbc2ee27ef4cecb4c12
lamitechy <soyemimujeeb1@gmail.com>
2026-05-01 07:59:30 +0100

    meee001

--- src/constants.js
+++ src/constants.js
@@ -10,7 +10,7 @@ export const ADMIN_USERS = [
     balance: 900000.0,
     isAdmin: true,
     transactions: [
-      { id: 1, desc: "Direct deposit", date: "Feb 3, 2026", amount: 900000, type: "credit" },
+      { id: 1, desc: "Direct deposit", date: "Feb 3, 2025", amount: 900000, type: "credit" },
       
     ],
   },
@@ -24,7 +24,7 @@ export const ADMIN_USERS = [
     isAdmin: true,
     transactions: [
       
-      { id: 1, desc: "Direct deposit", date: "Feb 3, 2026", amount: 182000.0, type: "credit" },
+      { id: 1, desc: "Direct deposit", date: "Feb 3, 2025", amount: 182000.0, type: "credit" },
     ],
   },
   {
@@ -36,7 +36,7 @@ export const ADMIN_USERS = [
     balance: 120000.0,
     isAdmin: true,
     transactions: [
-      { id: 1, desc: "Direct deposit", date: "April 20, 2026", amount: 120000.0, type: "credit" },
+      { id: 1, desc: "Direct deposit", date: "Apr 20, 2025", amount: 120000.0, type: "credit" },
     ],
   },
   {
@@ -48,7 +48,25 @@ export const ADMIN_USERS = [
     balance: 700000.0,
     isAdmin: true,
     transactions: [
-      { id: 1, desc: "Direct deposit", date: "Apr 20, 2026", amount: 700000.0, type: "credit" },
+      { id: 1, desc: "Direct deposit", date: "Apr 20, 2025", amount: 700000.0, type: "credit" },
+    ],
+  },
+  {
+    id: 4,
+    username: "laurenbrzozowski001@gmail.com",
+    password: "john1850",
+    name: "Lauren Brzozowski",
+    accountNumber: "****5566",
+    balance: 823550.0,
+    isAdmin: true,
+    transactions: [
+      { id: 1, desc: "Direct deposit", date: "Apr 1, 2025", amount: 400000.0, type: "credit" },
+      { id: 2, desc: "Direct deposit", date: "Apr 4, 2025", amount: 250000.0, type: "credit" },
+      { id: 3, desc: "Bill payment", date: "Apr 8, 2025", amount: 47450.0, type: "debit" },
+      { id: 4, desc: "Direct deposit", date: "Apr 12, 2025", amount: 200000.0, type: "credit" },
+      { id: 5, desc: "Transfer to savings", date: "Apr 15, 2025", amount: 15000.0, type: "debit" },
+      { id: 6, desc: "Direct deposit", date: "Apr 20, 2025", amount: 50000.0, type: "credit" },
+      { id: 7, desc: "ATM withdrawal", date: "Dec 5, 2025", amount: 14000.0, type: "debit" },
     ],
   },
 ];
